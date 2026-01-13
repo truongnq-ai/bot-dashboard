ffdb03727c3e45d30384e3d761d97dd843fb00a1
Truong Nguyen <truongnq.ai@gmail.com>
2026-01-13 18:05:43 +0700

    Update index.tsx

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,7 +1,43 @@
 import EyeIcon from "./eye.svg";
 import EyeCloseIcon from "./eye-close.svg";
 
+import GridIcon from "./grid.svg";
+import UserCircleIcon from "./user-circle.svg";
+import PageIcon from "./page.svg";
+import PlugInIcon from "./plug-in.svg";
+import GraduationCapIcon from "./graduation-cap.svg";
+import ChevronLeftIcon from "./chevron-left.svg";
+import ChevronDownIcon from "./chevron-down.svg";
+import HorizontaLDots from "./horizontal-dots.svg";
+
+import MoreDotIcon from "./more-dot.svg";
+import EnvelopeIcon from "./envelope.svg";
+import PlusIcon from "./plus.svg";
+import ArrowDownIcon from "./arrow-down.svg";
+import ArrowUpIcon from "./arrow-up.svg";
+import BoxIconLine from "./box-line.svg";
+import GroupIcon from "./group.svg";
+import CalenderIcon from "./calendar.svg";
+import TimeIcon from "./time.svg";
+
 export {
   EyeIcon,
   EyeCloseIcon,
+  GridIcon,
+  UserCircleIcon,
+  PageIcon,
+  PlugInIcon,
+  GraduationCapIcon,
+  ChevronLeftIcon,
+  ChevronDownIcon,
+  HorizontaLDots,
+  MoreDotIcon,
+  EnvelopeIcon,
+  PlusIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  BoxIconLine,
+  GroupIcon,
+  CalenderIcon,
+  TimeIcon,
 };
